857b1b286c026ae11ea974235614e2e3e05a4482
GitHub Action <action@github.com>
2025-05-10 14:48:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/fmesnata/spicedbschemalanguage/MyBundle.kt
+++ src/main/kotlin/com/github/fmesnata/spicedbschemalanguage/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.fmesnata.spicedbschemalanguage
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/fmesnata/spicedbschemalanguage/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/fmesnata/spicedbschemalanguage/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.fmesnata.spicedbschemalanguage.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
